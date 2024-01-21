b305ffb60af55a9fedf4433edf68c8412105738e
Armando Junior <armando.junior4440@gmail.com>
2024-01-21 21:26:09 +0000

    Configurando aplicação global da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
-import { NestFactory, Reflector } from '@nestjs/core'
+import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
-import { WrapperDataInterceptor } from '@modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor'
-import { NotFoundErrorFilter } from '@modules/shared-module/filters/not-found-error.filter'
-import { EntityValidationErrorFilter } from '@modules/shared-module/filters/entity-validation-error.filter'
+import { applyGlobalConfig } from '@modules/global-config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      errorHttpStatusCode: 422,
-    }),
-  )
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
-  app.useGlobalInterceptors(new WrapperDataInterceptor())
-  app.useGlobalFilters(
-    new NotFoundErrorFilter(),
-    new EntityValidationErrorFilter(),
-  )
+  applyGlobalConfig(app)
 
   await app.listen(3000)
 }
